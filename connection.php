9340489ef846a93af5c20e425adf96fef7425064
khadija-rahman <khadija.a13@gmail.com>
2018-05-20 10:08:19 +0100

    Added post and comment models

--- connection.php
+++ connection.php
@@ -1,22 +1,21 @@
 <?php
 
-/* 
- * copied from Peter's connection.php 
+/*
+ * copied from Peter's connection.php
  * Please change
  */
 
-
-
-class DB {
-    
-    private static $instance = NULL;
+class DB
+{
+    private static $instance = null;
 
     //Singleton Design Pattern
-    public static function getInstance() {
-      if (!isset(self::$instance)) {
-        $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
-        self::$instance = new PDO('mysql:host=localhost;dbname=php_mvc', 'root', '', $pdo_options);
-      }
-      return self::$instance;
+    public static function getInstance()
+    {
+        if (!isset(self::$instance)) {
+            $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
+            self::$instance = new PDO('mysql:host=library.cy8z5nl3gsit.eu-west-2.rds.amazonaws.com;dbname=rogue_blog_db', 'root', 'password', $pdo_options);
+        }
+        return self::$instance;
     }
 }
