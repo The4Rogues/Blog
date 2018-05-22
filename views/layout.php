92f8be39a198f602ef82b62d7c1b49b9c1e7097f
The4Rogues <38332543+The4Rogues@users.noreply.github.com>
2018-05-22 09:44:59 +0100

    change layout.php

--- views/layout.php
+++ views/layout.php
@@ -10,18 +10,16 @@
   <head>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
-<link rel="stylesheet" href="https://www.w3schools.com/lib/w3.css">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Tangerine">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Pacifico|Pangolin" >
-<link rel="stylesheet" href="views/css/styles.css">
-<title>Shopping Cart</title>
+
+<link rel="stylesheet" href="views/css/main.css">
+<title>Blog</title>
   </head>
   <body>
-    <header class="w3-container w3-gray">
+   <!-- <header class="w3-container w3-gray">
       <a href='/MVC_Skeleton'>Home</a>
       <a href='?controller=blog&action=readAll'>Products</a>
       <a href='?controller=product&action=create'>Add Product</a>
-    </header>
+    </header> -->
 <div class="w3-container w3-pink">
     <?php require_once('routes.php'); ?>
 </<div>
